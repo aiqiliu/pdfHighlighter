a5c7c3ab4031d5c90fbaae073d9d7d887d57cb4d
Aiqi Liu <aiqi@Aiqis-Mac-mini.local>
2016-02-16 16:09:40 -0600

    return all instances

--- highlight.py
+++ highlight.py
@@ -3,31 +3,48 @@ import pdfquery
 pdf = pdfquery.PDFQuery("test.pdf")
 
 pdf.load()
+x0 = 0
+x1 = 0
+x2 = 0
+x3 = 0
 #pageNum = pdf.doc.catalog['Pages'].resolve()['Count'] #get number of pages
 # print (pageNum)
 
 label = pdf.pq('LTTextLineHorizontal:contains("is")')
 
-x0 = float(label.attr('x0'))
-y0 = float(label.attr('y0'))
-x1 = float(label.attr('x1'))
-y1 = float(label.attr('y1'))
+# x0 = float(label.attr('x0'))
+# y0 = float(label.attr('y0'))
+# x1 = float(label.attr('x1'))
+# y1 = float(label.attr('y1'))
 
-box = [x0, y0, x1, y1]
-print(box)
-locate = pdf.pq('LTPage[page_index = 1]:in_bbox("90.0, 597.456, 533.542, 613.428")').text() 
-print(locate)
+# box = [x0, y0, x1, y1]
 
+#results = []
+#print(box)
 
-# cells = pdf.extract ([
-# 	('with_parent','LTPage[pageid=\'1\']'),
-#     ('cells', 'LTTextLineHorizontal:contains("is")')
+locate = pdf.pq('LTTextLineHorizontal:in_bbox("72.0, 548.456, 372.81, 571.256")').text() 
+#print(locate)
+search ='LTTextLineHorizontal:contains("is")'
+print(search)
 
-# 	]
-# )
+def getCoordinates(label):
+	x0 = float(label.attr('x0'))
+	y0 = float(label.attr('y0'))
+	x1 = float(label.attr('x1'))
+	y1 = float(label.attr('y1'))
+
+	box = [x0,x1,x2,x3]
+	return box
+
+cells = pdf.extract ([
+	('with_formatter', 'text'),
+	('with_parent','LTPage[page_index="0"]'),
+    ('cell', search)
+
+	])
 
 # #[cell.text.encode('utf-8').strip() for cell in cells['cells']]
-# print(cells)
+#print(cells)
 # label2 = pdf.pq('LTTextLineHorizontal:contains("ssme techniques can be")')
 # x00 = float(label.attr('x0'))
 # y00 = float(label.attr('y0'))
